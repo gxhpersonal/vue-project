fdbe8e45c5a85c99b4a1066da1782b2011ba1fbd
guoxh <guoxh@huizuche.com>
2018-12-26 18:51:35 +0800

    versions one

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,40 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-      </li>
-      <br>
-      <li>
-        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
-      </li>
-      <li>
-        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-      </li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
-      </li>
-    </ul>
+    <h2>持续开发中。。。</h2>
+    <router-link class="detail-btn" to="/detail">去详情页</router-link>
   </div>
 </template>
 
@@ -65,6 +33,22 @@ ul {
     a {
       color: #42b983;
     }
+  }
+}
+.hello {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  .detail-btn {
+    width: 200px;
+    height: 50px;
+    background: linear-gradient(90deg, #41b883, #35495e);
+    border-radius: 15px;
+    text-align: center;
+    line-height: 50px;
+    font-size: 20px;
+    color: #fff;
   }
 }
 </style>
