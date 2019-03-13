a72aeb28bd4e9b4d6b9b9eef5504213051259d87
guoxh <guoxuehui@51jiabo.com>
2019-03-13 16:30:23 +0800

    childcompone

---
+++ src/components/child.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>这是子组件</h2>
+        <h3>父组件传过来的值为：{{childData}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        childData:{}
+    }
+}
+</script>
+
--- src/components/detail.vue
+++ src/components/detail.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-      这是我的详情页
+      这是父组件
+      <childC :childData="Data"></childC>
   </div>
 </template>
+
+<script>
+import childC from "./child.vue";
+export default {
+  components:{
+    childC
+  },
+  data(){
+    return{
+      Data:{a:1,b:2}
+    }
+  }
+}
+</script>
