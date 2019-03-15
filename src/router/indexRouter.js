f88894e81cc55c103013faca7218024ab852c8ec
guoxh <guoxuehui@51jiabo.com>
2019-03-15 09:47:21 +0800

    hash

--- src/router/indexRouter.js
+++ src/router/indexRouter.js
@@ -6,7 +6,7 @@ const Detail = () => import("../components/detail/detail")
 Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  // mode: "history",
   routes: [
     {
       path: '/',
